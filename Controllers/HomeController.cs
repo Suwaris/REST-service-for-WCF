2a56db08034355918c4810b3aa07f54fbc4e2f4e
Ann <vansuwaris@gmail.com>
2023-10-11 12:52:58 +0530

    rest service only

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoRestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public async Task<string[]> Get()
+        {
+            ServiceReference1.AuthorServiceClient authorServiceClient = new ServiceReference1.AuthorServiceClient();
+            var data = await authorServiceClient.GetAuthorNamesAsync();
+            return data;
+        }
+    }
+}
